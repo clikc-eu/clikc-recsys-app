7ef7fcfca3c4d48f2821d6141c7b1042cfdd3049
antoniocasto <mail.antoniocasto@gmail.com>
2022-04-08 12:19:30 +0200

    Add authentication via API key

--- recsys-interface-service/app/api/service.py
+++ recsys-interface-service/app/api/service.py
@@ -1,5 +1,5 @@
 from typing import List
-from aiohttp import ClientResponse, ClientSession
+from aiohttp import ClientConnectionError, ClientResponse, ClientSession
 from .models import StatusOut
 from .constants import ModelServiceUrls
 import asyncio
@@ -9,23 +9,30 @@ from fastapi import HTTPException, status
 class RecsysInterfaceService():
 
     async def get_model_service_status(self, client_session: ClientSession):
-        async with client_session.get(
-            url=ModelServiceUrls.STATUS_URL
-        ) as response:
-            res = await response.json()
-            if response.status != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status, detail=res['detail'])
-            return await response.json()
+        try:
+            async with client_session.get(
+                url=ModelServiceUrls.STATUS_URL
+            ) as response:
+                res = await response.json()
+                if response.status != status.HTTP_200_OK:
+                    raise HTTPException(status_code=response.status, detail=res['detail'])
+                return await response.json()
+        except ClientConnectionError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service Not Available.")
 
 
     async def train_model(self, client_session: ClientSession):
-        async with client_session.post(
-            url=ModelServiceUrls.TRAIN_URL
-        ) as response:
-            res = await response.json()
-            if response.status != status.HTTP_202_ACCEPTED:
-                raise HTTPException(status_code=response.status, detail=res['detail'])
-            return await response.json()
+        try:
+            async with client_session.post(
+                url=ModelServiceUrls.TRAIN_URL
+            ) as response:
+                res = await response.json()
+                if response.status != status.HTTP_202_ACCEPTED:
+                    raise HTTPException(status_code=response.status, detail=res['detail'])
+                return await response.json()
+        except ClientConnectionError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service Not Available.")
+
 
 
     async def get_recommendations_for_new_user(self, user_features: List[str], num_pred: int, client_session: ClientSession):
@@ -38,15 +45,19 @@ class RecsysInterfaceService():
             'num_pred': num_pred
         }
 
-        async with client_session.post(
-            url=ModelServiceUrls.RECS_USER_FEATURES_URL,
-            json=payload,
-            params=params
-        ) as response:
-            res = await response.json()
-            if response.status != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status, detail=res['detail'])
-            return res
+        try:
+            async with client_session.post(
+                url=ModelServiceUrls.RECS_USER_FEATURES_URL,
+                json=payload,
+                params=params
+            ) as response:
+                res = await response.json()
+                if response.status != status.HTTP_200_OK:
+                    raise HTTPException(status_code=response.status, detail=res['detail'])
+                return res
+        except ClientConnectionError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service Not Available.")
+
 
 
     async def get_recommendations_for_user(self, user_id: int, num_pred: int, client_session: ClientSession):
@@ -54,15 +65,18 @@ class RecsysInterfaceService():
         params = {
             'num_pred': num_pred
         }
+        try:
+            async with client_session.get(
+                url=ModelServiceUrls.RECS_USER_URL + str(user_id),
+                params=params
+            ) as response:
+                res = await response.json()
+                if response.status != status.HTTP_200_OK:
+                    raise HTTPException(status_code=response.status, detail=res['detail'])
+                return res
+        except ClientConnectionError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service Not Available.")
 
-        async with client_session.get(
-            url=ModelServiceUrls.RECS_USER_URL + str(user_id),
-            params=params
-        ) as response:
-            res = await response.json()
-            if response.status != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status, detail=res['detail'])
-            return res
 
 
     async def get_similar_items(self, item_id: int, num_pred: int, client_session: ClientSession):
@@ -70,12 +84,15 @@ class RecsysInterfaceService():
         params = {
             'num_pred': num_pred
         }
+        try:
+            async with client_session.get(
+                url=ModelServiceUrls.RECS_ITEM_URL + str(item_id),
+                params=params
+            ) as response:
+                res = await response.json()
+                if response.status != status.HTTP_200_OK:
+                    raise HTTPException(status_code=response.status, detail=res['detail'])
+                return res
+        except ClientConnectionError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service Not Available.")
 
-        async with client_session.get(
-            url=ModelServiceUrls.RECS_ITEM_URL + str(item_id),
-            params=params
-        ) as response:
-            res = await response.json()
-            if response.status != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status, detail=res['detail'])
-            return res
